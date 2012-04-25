4468903297f2c6fba7fcd31046ef2701f718c2aa
Yu-Cheng Chuang <ducksteven@gmail.com>
2012-04-26 02:19:42 +0800

    let the popup window communicate to background page directly

--- copy-as-markdown.js
+++ copy-as-markdown.js
@@ -1,6 +1,21 @@
 var CopyAsMarkdown = new (function() {
   var resultContainer = document.getElementById("result-markdown");
 
+  var getCurrentTab = function (callback) {
+    chrome.tabs.query({
+      windowId: chrome.windows.WINDOW_ID_CURRENT,
+      active: true
+    }, function(tabs) {
+      callback(tabs[0]);
+    });
+  };
+
+  var getAllTabsOfCurrentWindow = function (callback) {
+    chrome.tabs.query({
+      windowId: chrome.windows.WINDOW_ID_CURRENT,
+    }, callback);
+  };
+
   var defaultTitle = "(No Title)";
 
   var setMarkdownResult = function(text) {
@@ -60,5 +75,30 @@ var CopyAsMarkdown = new (function() {
     setMarkdownResult(markdown);
     copyMarkdownCodeToClipboard();
   }
+
+  this.copyCurrentTab = function(options, callback) {
+    getCurrentTab(function(tab) {
+      // XXX: Bad namespacing! (CoffeeScript's binding can resolve this issue)
+      var markdown = CopyAsMarkdown.copyLinkAsMarkdown(tab.title, tab.url, options);
+      callback(markdown);
+    });
+  };
+
+  this.copyAllTabs = function(options, callback) {
+    getAllTabsOfCurrentWindow(function(tabs) {
+      var links = [];
+      for (var i in tabs) {
+        var tab = tabs[i];
+        links.push({
+          title: tab.title,
+          url: tab.url
+        });
+      };
+
+      // XXX: Bad namespacing! (CoffeeScript's binding can resolve this issue)
+      var markdown = CopyAsMarkdown.copyLinksAsListMarkdown(links, options);
+      callback(markdown);
+    });
+  };
 })();
 
