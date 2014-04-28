9884463f7c69232b539e340e88ca04560ff2fc7e
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-04-28 20:07:22 +0800

    init project using cfx tool and import code from official guide

---
+++ src-fx/lib/main.js
@@ -0,0 +1,17 @@
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+
+var button = buttons.ActionButton({
+  id: "mozilla-link",
+  label: "Visit Mozilla",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: handleClick
+});
+
+function handleClick(state) {
+  tabs.open("http://www.mozilla.org/");
+}
