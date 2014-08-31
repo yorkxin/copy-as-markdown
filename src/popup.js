bb414a890c888ad865ee7d8f1f3260d8b58ae0a0
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-08-31 12:45:14 +0800

    show all tabs count in popup window

--- src/popup.js
+++ src/popup.js
@@ -3,4 +3,9 @@
   ViewController.bindFeature("current-tab-link", "click", "copyCurrentTab");
   ViewController.bindFeature("all-tabs-link-as-list", "click", "copyAllTabs");
 
+  chrome.windows.getCurrent({ populate: true }, function(crWindow) {
+    var tabsCount = crWindow.tabs.length;
+    document.getElementById("count-of-all-tabs").textContent = tabsCount;
+  });
+
 })(ViewController);
