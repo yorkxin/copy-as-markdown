b24165fa032ea532303bba314b0f537d3e398dd3
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-11-08 22:33:14 +0800

    style popup menu like chrome version

--- src-fx/data/panel.js
+++ src-fx/data/panel.js
@@ -2,8 +2,13 @@ var commandDispatcher = function(event) {
   var button = event.target;
   self.port.emit(button.dataset.command, button.dataset.scope);
 
+  button.classList.add('highlight-success');
+
   // close panel after command
-  self.port.emit("close");
+  setTimeout(function() {
+    self.port.emit("close");
+    button.classList.remove('highlight-success')
+  }, 300);
 };
 
 var buttons = document.querySelectorAll("button[data-command]");
