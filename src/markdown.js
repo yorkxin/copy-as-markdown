1629fb1923851c93a77a7c290fad969b46744e66
Yu-Cheng Chuang <ducksteven@gmail.com>
2016-01-08 21:16:48 +0800

    fix the bug that link title is not escaped

--- src/markdown.js
+++ src/markdown.js
@@ -1,7 +1,15 @@
+const ESCAPE_CHARS = /([\\`*_\[\]#])/g;
+
+var escapeLinkText = function(text) {
+  return text.replace(ESCAPE_CHARS, "\\$1");
+};
+
 var Markdown = {
   linkTo: function(title, url) {
     if (title === undefined) {
       title = CopyAsMarkdown.getDefaultTitle();
+    } else {
+      title = escapeLinkText(title);
     }
 
     var result = "[" + title + "](" + url + ")";
