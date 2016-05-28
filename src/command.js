f07abad37d63b40c11472dc6e95c050c8530d0a4
ijump@mac <ijumps@users.noreply.github.com>
2016-05-29 00:21:05 +0800

    Add command for selected-markdown

--- src/command.js
+++ src/command.js
@@ -11,5 +11,18 @@ chrome.commands.onCommand.addListener(function(command) {
     case "highlighted-tabs-link-as-list":
       CopyAsMarkdown.copyHighlightedTabs();
       break;
+
+    case "selected-markdown":
+      chrome.tabs.query({
+        windowId: chrome.windows.WINDOW_ID_CURRENT,
+        active: true
+      }, function(tabs) {
+        chrome.tabs.sendRequest(tabs[0].id, {}, function(selection) {
+        if (selection !== undefined) {
+          CopyAsMarkdown.copySelection(selection.html, selection.text);
+        }
+        });
+      });
+      break;
   }
 });
--- src/markdown.js
+++ src/markdown.js
@@ -16,7 +16,41 @@ var Markdown = {
       return text;
     }
     else {
-      return toMarkdown(html);
+      var converter = [
+        {
+          filter: 'span',
+          replacement: function (innerHTML) {
+            return innerHTML;
+          }
+        },
+        {
+          filter: 'div',
+          replacement: function (content) {
+            return '\n\n' + content + '\n\n';
+          }
+        },
+        {
+          filter: function (node) {
+            return node.nodeName === 'PRE' && node.firstChild.nodeName !== 'CODE';
+          },
+          replacement: function(content) {
+            return '\n\n    ' + content.replace(/\n/g, '\n    ') + '\n\n';
+          }
+        },
+        {
+          filter: ['section', 'small', 'font'],
+          replacement: function (content) {
+            return content;
+          }
+        },
+        {
+          filter: ['iframe', 'javascript', 'script'],
+          replacement: function (content) {
+            return '';
+          }
+        }
+      ];
+      return toMarkdown(html, { gfm: true, converters: converter });
     }
   }
 };
