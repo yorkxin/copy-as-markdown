73def81b64cb1b383d50e2929253be8e1abef869
Yu-Cheng Chuang <ducksteven@gmail.com>
2017-06-05 17:04:16 +0900

    Refactor markdown.js to module

--- common/markdown.js
+++ common/markdown.js
@@ -5,30 +5,32 @@ var escapeLinkText = function(text) {
   return text.replace(ESCAPE_CHARS, "\\$1");
 };
 
-var Markdown = {
-  linkTo: function(title, url, options) {
-    options = options || {};
+export linkTo(title, url, options) {
+  options = options || {};
 
-    // used for copying link-in-image
-    if (options.escape !== true) {
-      options.escape = false;
-    }
-
-    if (title === undefined) {
-      title = DEFAULT_TITLE;
-    }
-
-    if (options.escape) {
-      title = escapeLinkText(title);
-    }
-
-    var result = "[" + title + "](" + url + ")";
-
-    return result;
-  },
-  imageFor: function(title, url) {
-    return "!["+title+"]("+url+")";
+  // used for copying link-in-image
+  if (options.escape !== true) {
+    options.escape = false;
   }
+
+  if (title === undefined) {
+    title = DEFAULT_TITLE;
+  }
+
+  if (options.escape) {
+    title = escapeLinkText(title);
+  }
+
+  var result = "[" + title + "](" + url + ")";
+
+  return result;
 };
 
-export default Markdown;
+export imageFor(title, url) {
+  return "!["+title+"]("+url+")";
+}
+
+export default {
+  linkTo,
+  imageFor
+};
