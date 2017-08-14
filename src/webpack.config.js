628fe96afe7f9d781ae8bd49b447162757672603
Yu-Cheng Chuang <ducksteven@gmail.com>
2017-08-14 22:48:26 +0900

    support build target

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,6 +1,14 @@
 const path = require("path");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+if (!process.env.TARGET) {
+  throw Error("Please specify env var TARGET, 'chrome' or 'firefox'.")
+} else if (!(process.env.TARGET === 'chrome' || process.env.TARGET === 'firefox')) {
+  throw Error("TARGET can only be 'chrome' or 'firefox'.")
+} else {
+  console.info(`\x1b[1;32mBuilding for target ${process.env.TARGET}...\x1b[m`)
+}
+
 module.exports = {
   entry: {
     background: [
@@ -12,7 +20,7 @@ module.exports = {
     popup: "./ui/popup.js"
   },
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "build", process.env.TARGET),
     filename: "[name].dist.js"
   },
   resolve: {
