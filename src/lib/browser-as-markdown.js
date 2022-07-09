95568b02997dba480f6e3a0eb5fbc9a9c0ad466d
Yucheng Chuang <ducksteven@gmail.com>
2022-07-09 15:25:45 +0900

    Firefox compatibility

--- src/lib/browser-as-markdown.js
+++ src/lib/browser-as-markdown.js
@@ -2,6 +2,31 @@
 import * as Markdown from './markdown.js';
 
 /**
+ * Async wrapper around chrome.tabs.query function.
+ *
+ * This is a workaround for Firefox (102), whose `chrome.tabs.query` was not properly implemented.
+ * `await browser.tabs.query()` works though, but `browser` object is not compatible with Chromium.
+ *
+ * @param query {chrome.tabs.QueryInfo}
+ * @returns {Promise<chrome.tabs.Tab[]>}
+ */
+async function asyncTabsQuery(query) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.tabs.query(query, (tabs) => {
+        if (!tabs) {
+          reject(new Error(`got nil (${typeof tabs})`));
+        } else {
+          resolve(tabs);
+        }
+      });
+    } catch (e) {
+      reject(new Error('tabs.query failed:', { cause: e }));
+    }
+  });
+}
+
+/**
  *
  * @param info {chrome.contextMenus.OnClickData}
  * @returns {string}
@@ -28,38 +53,42 @@ export function onClickLink(info) {
 export async function handleExport(action) {
   switch (action) {
     case 'current-tab-link': {
-      const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+      const tabs = await asyncTabsQuery({ currentWindow: true, active: true });
+      if (tabs.length !== 1) {
+        throw new Error(`Expecting exactly 1 tab, got ${tabs.length} items.`);
+      }
+
       const onlyOneTab = tabs[0];
       return Markdown.linkTo(onlyOneTab.title, onlyOneTab.url);
     }
 
     case 'all-tabs-link-as-list': {
-      const tabs = await chrome.tabs.query({ currentWindow: true });
+      const tabs = await asyncTabsQuery({ currentWindow: true });
       return Markdown.links(tabs, {});
     }
 
     case 'all-tabs-title-as-list': {
-      const tabs = await chrome.tabs.query({ currentWindow: true });
+      const tabs = await asyncTabsQuery({ currentWindow: true });
       return Markdown.list(tabs.map((tab) => tab.title));
     }
 
     case 'all-tabs-url-as-list': {
-      const tabs = await chrome.tabs.query({ currentWindow: true });
+      const tabs = await asyncTabsQuery({ currentWindow: true });
       return Markdown.list(tabs.map((tab) => tab.url));
     }
 
     case 'highlighted-tabs-link-as-list': {
-      const tabs = await chrome.tabs.query({ currentWindow: true, highlighted: true });
+      const tabs = await asyncTabsQuery({ currentWindow: true, highlighted: true });
       return Markdown.links(tabs, {});
     }
 
     case 'highlighted-tabs-title-as-list': {
-      const tabs = await chrome.tabs.query({ currentWindow: true, highlighted: true });
+      const tabs = await asyncTabsQuery({ currentWindow: true, highlighted: true });
       return Markdown.list(tabs.map((tab) => tab.title));
     }
 
     case 'highlighted-tabs-url-as-list': {
-      const tabs = await chrome.tabs.query({ currentWindow: true, highlighted: true });
+      const tabs = await asyncTabsQuery({ currentWindow: true, highlighted: true });
       return Markdown.list(tabs.map((tab) => tab.url));
     }
 
