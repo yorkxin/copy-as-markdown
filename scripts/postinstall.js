f9a727a1f9d9e985b56d5505aa789922ab6f0526
Yucheng Chuang <ducksteven@gmail.com>
2024-06-29 11:45:04 +0900

    Build on Windows

---
+++ scripts/postinstall.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+const files = [
+  'node_modules/turndown/dist/turndown.js',
+  'node_modules/bulma/css/bulma.css',
+  'node_modules/webextension-polyfill/dist/browser-polyfill.js',
+];
+
+const dir = path.join(import.meta.dirname, '..', 'src/vendor');
+
+files.forEach((file) => {
+  const filename = path.basename(file);
+  fs.copyFileSync(file, path.join(dir, filename));
+});
