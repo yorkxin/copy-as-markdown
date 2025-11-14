fd028a34f51b971cd88b54feb47e81e382ab6f89
Yucheng Chuang <ducksteven@gmail.com>
2025-11-14 23:26:51 +0900

    eslint

--- scripts/build-test-extension.js
+++ scripts/build-test-extension.js
@@ -18,7 +18,6 @@ const __dirname = path.dirname(__filename);
 const rootDir = path.join(__dirname, '..');
 
 const sourceDir = path.join(rootDir, 'chrome');
-const targetDir = path.join(rootDir, 'chrome-test');
 
 const variants = [
   {
