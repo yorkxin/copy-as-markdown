5e21009b7042c39c0482b5804bf2d4082e291f90
Yucheng Chuang <ducksteven@gmail.com>
2025-11-27 00:00:07 +0900

    Remove uhtml

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,17 +3,16 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
-const vendorDir = path.join(import.meta.dirname, '..', 'src/vendor');
-
 const files = [
-  { src: 'node_modules/turndown/dist/turndown.js' },
-  { src: 'node_modules/bulma/css/bulma.css' },
-  { src: 'node_modules/webextension-polyfill/dist/browser-polyfill.js' },
-  { src: 'node_modules/mustache/mustache.mjs' },
-  { src: 'node_modules/uhtml/index.js', dest: 'uhtml.js' },
+  'node_modules/turndown/dist/turndown.js',
+  'node_modules/bulma/css/bulma.css',
+  'node_modules/webextension-polyfill/dist/browser-polyfill.js',
+  'node_modules/mustache/mustache.mjs',
 ];
 
-files.forEach(({ src, dest }) => {
-  const filename = dest ?? path.basename(src);
-  fs.copyFileSync(src, path.join(vendorDir, filename));
+const dir = path.join(import.meta.dirname, '..', 'src/vendor');
+
+files.forEach((file) => {
+  const filename = path.basename(file);
+  fs.copyFileSync(file, path.join(dir, filename));
 });
