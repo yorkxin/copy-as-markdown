ac17cb15bed223f4de8a891e2cf24bbd5aa70ca8
Yu-Cheng Chuang <ducksteven@gmail.com>
2026-06-21 20:46:20 +0900

    chore(e2e): remove unused time and ActionChains imports from conftest

--- e2e_test/conftest.py
+++ e2e_test/conftest.py
@@ -1,12 +1,10 @@
 import re
 import sys
 from textwrap import dedent
-import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
